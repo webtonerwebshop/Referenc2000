ad8e897de3d831cdb7a0392d8359869c9d7999af
webtonerwebshop <121196115+webtonerwebshop@users.noreply.github.com>
2022-12-21 20:51:15 +0100

    Add files via upload
    fdsfs

---
+++ commonlogic.js
@@ -0,0 +1,33 @@
+var openorclosed = 0;
+
+function navdisplay() {
+    openorclosed+=1;
+    if (openorclosed % 2 == 1) {
+        document.getElementById("mobile_navbar").style.opacity = 1;
+    }
+    else {
+        document.getElementById("mobile_navbar").style.opacity = 0;
+    }
+}
+
+
+window.addEventListener("scroll", reveal);
+
+function reveal () {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++){
+
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 50;
+
+        if (revealTop <windowHeight - revealPoint){
+            reveals[i].classList.add("active");
+        }
+        /*else {
+            reveals[i].classList.remove("active");
+
+        }*/
+    }
+}
