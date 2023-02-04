693a27af5137c3bf660f4a7bb9d51e9e3905eabc
webtonerwebshop <webtonerwebshop@gmail.com>
2023-02-04 13:35:29 +0100

    ada

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@ if (isset($_POST['submit'])) {
   $message = $_POST['message'];
 
   // Set the recipient email address
-  $to = 'webtonerwebshop@gmail.com';
+  $to = 'kapcsolat@referenc2000.hu';
 
   // Set the email subject
   $subject = 'New message from contact form';
