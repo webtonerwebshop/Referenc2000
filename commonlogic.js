b16c44cca72fa2b89784b880ace857cfbdc32650
webtonerwebshop <webtonerwebshop@gmail.com>
2023-02-04 19:05:30 +0100

    s

---
+++ commonlogic.js
@@ -0,0 +1,38 @@
+var openorclosed = 1;
+
+function navdisplay() {
+    openorclosed+=1;
+    if (openorclosed % 2 == 1) {
+        document.getElementById("navbar").style.display = "block";
+    }
+    else {
+        document.getElementById("navbar").style.display = "none";
+    }
+}
+
+
+var openorclosed_rooms = 1;
+
+function navdisplay_rooms() {
+    openorclosed_rooms+=1;
+    if (openorclosed_rooms % 2 == 1) {
+        document.getElementById("rooms").style.display = "block";
+    }
+    else {
+        document.getElementById("rooms").style.display = "none";
+    }
+}
+
+
+
+var openorclosed_services = 1;
+
+function navdisplay_services() {
+    openorclosed_services+=1;
+    if (openorclosed_services % 2 == 1) {
+        document.getElementById("services").style.display = "block";
+    }
+    else {
+        document.getElementById("services").style.display = "none";
+    }
+}
